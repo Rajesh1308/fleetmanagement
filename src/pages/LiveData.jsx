238f2758ffb42c5a28cb937ad6783296a522c041
Rajesh1308 <rajesharulrathna@gmail.com>
2024-11-21 04:52:55 +0530

    Destination Chennai updated

--- src/pages/LiveData.jsx
+++ src/pages/LiveData.jsx
@@ -31,8 +31,8 @@ const LiveData = () => {
     const handleVehicleTypeChange = (e) => setVehicleType(e.target.value);
     const handleVehicleIdChange = (e) => setVehicleId(e.target.value);
 
-    const destinationName = useState("Ajmer");
-    const [destination, setDestination] = useState({ lat: 26.4499, lng: 74.6399 }) // Ajmer GPS coordinates
+    const destinationName = useState("Chennai");
+    const [destination, setDestination] = useState({ lat: 13.0843, lng: 80.2705 }) // Chennai GPS coordinates
 
     useEffect(() => {
         const loadTopic = `fleet/${vehicleType}/${vehicleId}/load`;
